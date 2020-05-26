b68cbfcb257abcf4b48a87d85259afad02f6182e
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-26 11:01:44 -0500

    init commit

---
+++ theSpringBootApparel/src/test/java/com/example/theSpringBootApparel/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theSpringBootApparel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
