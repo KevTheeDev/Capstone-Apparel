0e56be08e6be1ed39e0416a287436bd913a79ef8
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-28 09:12:34 -0500

    added import and empty state and class

---
+++ thee_apparel/src/components/CartContainer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class CartContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            cart: []
+         };
+    }
+    render() {
+        return (
+            <div> 
+                
+            </div>
+        );
+    }
+}
+
+export default CartContainer;
