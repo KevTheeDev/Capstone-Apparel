3f886e7f0d27acf7e60ae841b23c0ff579c99be6
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-28 18:04:07 -0500

    created contactpg.jsx, added a function and text

---
+++ thee_apparel/src/components/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ContactPage(){
+    return(
+        <React.Fragment>
+            <h3> Contact Us </h3>
+        </React.Fragment>
+    )
+}
+
+export default ContactPage;
