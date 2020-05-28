25cc2acb83c8e1d84192e2a29a510cae2f0e0256
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-28 09:10:51 -0500

    from js to jsx file

---
+++ thee_apparel/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import HomePageCss from '../styles/HomePage.css'
+
+function HomePage() {
+        return (
+            <div className="homepage">
+                <p className="homepage-paragraph"> Clothes For The Comfortable Stage of Life </p>
+
+                <p id="firstpg-paragraph"> ...comfort of dad / mom jeans with the "hipness" of modern times. Express yourself, through various styles, just a little, you deserve it! </p>
+
+            </div>
+        );
+}
+
+export default HomePage;
